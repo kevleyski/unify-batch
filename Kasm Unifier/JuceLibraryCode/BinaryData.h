cb6c6ff201175e786c7659b70fb7eb52474ea5fa
Kevin Staunton-Lambert <kevstal@meta.com>
2024-07-15 21:10:16 +1000

    KJSL: Unify patch generator tools macOS adding Unify Layer presets adding MIDI Controllers

---
+++ Kasm Unifier/JuceLibraryCode/BinaryData.h
@@ -0,0 +1,99 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   One_Serum_Layer_xml;
+    const int            One_Serum_Layer_xmlSize = 13379;
+
+    extern const char*   One_HALion7_Layer_xml;
+    const int            One_HALion7_Layer_xmlSize = 96964;
+
+    extern const char*   One_Diva_Layer_xml;
+    const int            One_Diva_Layer_xmlSize = 26236;
+
+    extern const char*   One_Zebra2_Layer_xml;
+    const int            One_Zebra2_Layer_xmlSize = 96823;
+
+    extern const char*   One_ZebraHZ_Layer_xml;
+    const int            One_ZebraHZ_Layer_xmlSize = 117497;
+
+    extern const char*   One_Zebralette_Layer_xml;
+    const int            One_Zebralette_Layer_xmlSize = 22652;
+
+    extern const char*   One_Zebralette3_Layer_xml;
+    const int            One_Zebralette3_Layer_xmlSize = 226027;
+
+    extern const char*   One_Spire_Layer_xml;
+    const int            One_Spire_Layer_xmlSize = 14289;
+
+    extern const char*   One_Twin3_Layer_xml;
+    const int            One_Twin3_Layer_xmlSize = 17315;
+
+    extern const char*   One_Pigments_Layer_xml;
+    const int            One_Pigments_Layer_xmlSize = 438882;
+
+    extern const char*   One_Kontakt_Layer_xml;
+    const int            One_Kontakt_Layer_xmlSize = 125419;
+
+    extern const char*   One_Omnisphere_Multi_Layer_xml;
+    const int            One_Omnisphere_Multi_Layer_xmlSize = 650674;
+
+    extern const char*   One_Omnisphere_Patch_Layer_xml;
+    const int            One_Omnisphere_Patch_Layer_xmlSize = 653388;
+
+    extern const char*   One_Battalion_Layer_xml;
+    const int            One_Battalion_Layer_xmlSize = 299285;
+
+    extern const char*   One_Lion_Layer_xml;
+    const int            One_Lion_Layer_xmlSize = 32634;
+
+    extern const char*   One_Massive_X_Layer_xml;
+    const int            One_Massive_X_Layer_xmlSize = 57760;
+
+    extern const char*   One_Decent_Sampler_Layer_xml;
+    const int            One_Decent_Sampler_Layer_xmlSize = 263257;
+
+    extern const char*   One_Vital_Layer_xml;
+    const int            One_Vital_Layer_xmlSize = 317253;
+
+    extern const char*   One_Reason_Rack_Layer_xml;
+    const int            One_Reason_Rack_Layer_xmlSize = 32966;
+
+    extern const char*   One_UVIWorkstation_Layer_xml;
+    const int            One_UVIWorkstation_Layer_xmlSize = 17388;
+
+    extern const char*   One_SurgeXT_Layer_xml;
+    const int            One_SurgeXT_Layer_xmlSize = 98707;
+
+    extern const char*   One_Sylenth1_Layer_xml;
+    const int            One_Sylenth1_Layer_xmlSize = 241970;
+
+    extern const char*   One_Softube_Modular_Layer_xml;
+    const int            One_Softube_Modular_Layer_xmlSize = 20896;
+
+    extern const char*   One_Lush2_Layer_xml;
+    const int            One_Lush2_Layer_xmlSize = 220373;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 24;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
